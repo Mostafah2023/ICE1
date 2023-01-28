8f8235b66ae92b980e2a32ce49cbb1a50d3e1893
Mostafah2023 <122639015+Mostafah2023@users.noreply.github.com>
2023-01-27 22:24:16 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -60,19 +60,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Mostafa Hussain, but you can call me Moose or Moe.");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("Where I see myself in 5 years?");
+        System.out.println("-- I see myself working for a great Software Dev company");
+        System.out.println("-- Job security and pay are the two important factors for me");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Being virtually creative (App dev/Web dev)");
         System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Watching Youtube");
+        System.out.println("-- Gaming");
 
         System.out.println();
         
